26083aaf6d3fab103ffbe5af655d32677d560985
shervin210 <152472820+shervin210@users.noreply.github.com>
2025-08-04 23:04:05 +0330

    explore project section

--- infrafund/src/components/header.tsx
+++ infrafund/src/components/header.tsx
@@ -1,14 +1,33 @@
+"use client";
+
 import { Bell, Headset, Wallet } from "lucide-react";
 import React from "react";
+import { usePathname } from "next/navigation";
 import { CustomButton } from "./ui/custom-button";
 
+const routeTitles: Record<string, string> = {
+  "/": "Dashboard",
+  "/kyc": "KYC",
+  "/explore-projects": "Explore Projects",
+  "/projects": "Projects",
+  "/settings": "Settings",
+};
+
 export default function Header() {
+  const pathname = usePathname();
+
+  const pageTitle = routeTitles[pathname] || "Page";
+
   return (
-    <div className="flex h-16 shrink-0 justify-between items-center  sticky top-0 z-20 rounded-lg mb-4 ">
-      <div className="flex flex-col gap-2\">
-        <span className="text-sm font-normal text-white">Hi sherv</span>
-        <span className="text-[40px] font-bold text-white">KYC</span>
+    <div className="flex h-16 shrink-0 justify-between items-center sticky top-0 z-20 rounded-lg mb-4">
+      <div className="flex flex-col gap-2">
+        <span className="text-sm font-normal text-white leading-2">
+          Hi sherv
+          <span className="text-[#8087A3] text-base font-normal"> - Guest</span>
+        </span>
+        <span className="text-[40px] font-bold text-white">{pageTitle}</span>
       </div>
+
       <div className="flex justify-center items-center gap-4">
         <Headset size={24} className="text-white cursor-pointer" />
         <Bell size={24} className="text-white cursor-pointer" />
@@ -19,7 +38,7 @@ export default function Header() {
           <Wallet size={24} />
           <span className="text-sm font-semibold">Connect Wallet</span>
         </CustomButton>
-        <div className="w-12 h-12 rounded-full bg-[#263247] flex justify-center items-center">
+        <div className="w-12 h-12 rounded-full bg-[#263247] flex justify-center items-center text-white">
           S
         </div>
       </div>
